9bb1af4c8f12d9288437da8e16481c65c119180f
Jae Min Choi <jaeminche@gmail.com>
2017-10-27 00:42:25 +0900

    docs: Add documentations

--- designs.js
+++ designs.js
@@ -2,6 +2,7 @@
 const pixelCanvas = document.getElementById("pixel_canvas");
 const formElmt = document.getElementById("sizePicker");
 
+/** @description Makes grid taking user's inputs as height and width */
 function makeGrid() {
     event.preventDefault();
     const height = document.getElementById("input_height").value;
@@ -17,14 +18,16 @@ function makeGrid() {
     pixelCanvas.innerHTML = grid;
 }
 
+/** @description Picks color  */
 function colorGrid() {
     event.preventDefault();
-
     $("td").click(function() {
         const colorPicked = document.getElementById("colorPicker").value;
         $(this).css("background", colorPicked);
     });
 }
 
+// call the function 'makeGrid', if user clicks the submit button
 formElmt.addEventListener("submit", makeGrid);
+// Call the function 'colorGrid', if mouse pointer is on the canvas
 pixelCanvas.addEventListener("mouseover", colorGrid);
