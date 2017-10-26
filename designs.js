e0dd98fd39867cd3c2862b1c370b66bd8bbf54d9
Jae Min Choi <jaeminche@gmail.com>
2017-10-27 00:58:31 +0900

    style: Replace all ""s to ''s

--- designs.js
+++ designs.js
@@ -1,19 +1,19 @@
 // Select canvas and form elements
-const pixelCanvas = document.getElementById("pixel_canvas");
-const formElmt = document.getElementById("sizePicker");
+const pixelCanvas = document.getElementById('pixel_canvas');
+const formElmt = document.getElementById('sizePicker');
 
 /** @description Makes grid taking user's inputs as height and width */
 function makeGrid() {
     event.preventDefault();
-    const height = document.getElementById("input_height").value;
-    const width = document.getElementById("input_width").value;
-    let grid = "";
+    const height = document.getElementById('input_height').value;
+    const width = document.getElementById('input_width').value;
+    let grid = '';
     for (let i = 0; i < height; i++) {
-        grid += "<tr>";
+        grid += '<tr>';
         for (let s = 0; s < width; s++) {
-            grid += "<td></td>";
+            grid += '<td></td>';
         }
-        grid += "</tr>";
+        grid += '</tr>';
     }
     pixelCanvas.innerHTML = grid;
 }
@@ -21,13 +21,13 @@ function makeGrid() {
 /** @description Picks color  */
 function colorGrid() {
     event.preventDefault();
-    $("td").click(function() {
-        const colorPicked = document.getElementById("colorPicker").value;
-        $(this).css("background", colorPicked);
+    $('td').click(function() {
+        const colorPicked = document.getElementById('colorPicker').value;
+        $(this).css('background', colorPicked);
     });
 }
 
 // call the function 'makeGrid', if user clicks the submit button
-formElmt.addEventListener("submit", makeGrid);
+formElmt.addEventListener('submit', makeGrid);
 // Call the function 'colorGrid', if mouse pointer is on the canvas
-pixelCanvas.addEventListener("mouseover", colorGrid);
+pixelCanvas.addEventListener('mouseover', colorGrid);
